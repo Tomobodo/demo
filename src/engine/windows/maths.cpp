6a84b8e4b845f734bd46b929a1249bc5aa4df610
Thomas BAUDON <baudon.thomas@gmail.com>
2026-03-10 23:12:35 +0100

    exploring a way to render text but too much code due to complexity, ending up outweighting a basic bitmap font

--- src/engine/windows/maths.cpp
+++ src/engine/windows/maths.cpp
@@ -1,4 +1,5 @@
 ﻿#include "engine/maths.hpp"
+#include <immintrin.h>
 
 namespace engine
 {
@@ -16,5 +17,28 @@ namespace engine
 	float cos(float angle)
 	{
 		return sin(angle + PI / 2.0f);
+	}
+
+	float sqrt(float x)
+	{
+		__m128 v = _mm_set_ss(x);
+		v = _mm_sqrt_ss(v);
+		return _mm_cvtss_f32(v);
+	}
+
+	float min(float a, float b)
+	{
+		return (a < b) ? a : b;
+	}
+
+	float max(float a, float b)
+	{
+		return (a > b) ? a : b;
+	}
+
+	float clamp(float v, float low, float high)
+	{
+		const float t = v < low ? low : v;
+		return t > high ? high : t;
 	}
 }
